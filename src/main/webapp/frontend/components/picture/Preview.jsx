c9ae6391e9ae2c81bd4ef2b38547c8d9b4160e69
Hanna Sira <Hanna_Sira@epam.com>
2017-12-14 07:28:31 +0200

    try react-custom-scrollbars

--- src/main/webapp/frontend/components/picture/Preview.jsx
+++ src/main/webapp/frontend/components/picture/Preview.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, {keyframes} from 'styled-components';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './preview.less';
 import PreviewImageFrame from "./PreviewImageFrame";
 import constants from './styles';
@@ -12,21 +13,31 @@ const increase = (props) => keyframes`
    0% {
       height: 0;
       width: 0;
+      background-color: transparent;
   }
   50% {
       height: 90%;
       width: ${props.imageWidth};
+      background-color: transparent;  
+  }
+  100% {
+      background-color: ${constants.preview_frame_color};
   }
 `
 
 const decrease = (props) => keyframes`
+   0% {
+      background-color: ${constants.preview_frame_color};
+   }
    50% {
       height: 90%;
       width: ${props.imageWidth};
+      background-color: transparent;
   }
   100% {
       height: 0;
       width: 0;
+      background-color: transparent;
   }
 `
 
@@ -46,8 +57,11 @@ const Modal = styled.div`
 
 class Preview extends React.Component {
 
-  constructor(props) {
+
+
+    constructor(props) {
     super(props);
+
 
     this.animationTime = parseInt(constants['preview_animation_duration']);
 
@@ -84,9 +98,33 @@ class Preview extends React.Component {
     });
   }
 
+
+    renderThumb({ style, ...props }) {
+        const thumbStyle = {
+            backgroundColor: 'white',
+            height: '70%'
+        };
+        return (
+            <div
+                style={{ ...style, ...thumbStyle }}
+                {...props}/>
+        );
+    }
+
   render() {
     const {animate, imageWidth} = this.state;
     const {images, src} = this.props;
+    let bottomWidthWithoutPaddings = imageWidth;
+
+
+        const bottom = document.getElementById('bottom');
+        if(bottom) {
+            const style = bottom.currentStyle || window.getComputedStyle(bottom);
+            const padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+            bottomWidthWithoutPaddings = imageWidth - padding;
+
+        }
+
 
     return (
         <Modal className={`magnify_modal ${animate}`}
@@ -99,9 +137,18 @@ class Preview extends React.Component {
                                close={() => this.close()}
                                clearAnimate={() => this.clearAnimate()}
             />
-            <BottomSection className='magnify_modal_img_frame_bottom' imageWidth={imageWidth}>
+
+            <BottomSection id='bottom' className='magnify_modal_img_frame_bottom' imageWidth={imageWidth}>
+              <Scrollbars
+                  autoHide={false}
+                  autoHeight={true}
+                  style={{ width: bottomWidthWithoutPaddings, height: '100%' }}
+                  renderThumbVertical={this.renderThumb}
+                  renderTrackHorizontal={props => <div {...props} style={{display: 'none'}}/>}
+                  renderThumbHorizontal={props => <div {...props} style={{display: 'none'}}/>}>
               <div className='magnify_modal_img_frame_bottom_caption'>Caption</div>
               <div className='magnify_modal_img_frame_bottom_text'>Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason. Some text which was provided for some reason.</div>
+              </Scrollbars>
             </BottomSection>
           </div>
         </Modal>
--- src/main/webapp/frontend/components/picture/styles.js
+++ src/main/webapp/frontend/components/picture/styles.js
@@ -1,6 +1,7 @@
 const constants = {
     'preview_animation_duration': '4s',
-    'picture_animation_duration': '1s'
+    'picture_animation_duration': '1s',
+    'preview_frame_color': 'black'
 };
 
 module.exports = constants;
